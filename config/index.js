537255cc571fd852352d7b096dd73eac42872ded
rootchuan <112492525+rootchuan@users.noreply.github.com>
2022-08-30 22:03:57 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -4,13 +4,13 @@ export const config = {
      */
 
     // 公众号APP_ID
-    APP_ID: "",
+    APP_ID: "wxa91ae2ddcd10af96",
     // 公众号APP_SECRET
-    APP_SECRET: "",
+    APP_SECRET: "bf68afd83bf825a3212b2838fbcdce31",
     // 模板消息id
-    TEMPLATE_ID: "",
+    TEMPLATE_ID: "oow_t6p7KYY5OGOzdhwK20uARXiM",
     // 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如["wx1", "wx2"]
-    USERS: [""],
+    USERS: ["R7HicNUVVTfjZ-1iDNrO0YQFdexz_ibSjBTRaK6X9Qc"],
      
     /**
      * 信息配置
